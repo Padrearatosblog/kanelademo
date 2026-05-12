9e5b28ef42500967d17a439d3bf1d42053afce44
bryans.quiroz <bryans.astorga@gmail.com>
2026-05-12 17:43:43 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
-const elements = document.querySelectorAll(
-  ".intro, .section-heading, .plate, .drink-text, .drink-img, .quote, .location > div"
-);
-
-elements.forEach((element) => {
-  element.classList.add("reveal");
-});
+const reveals = document.querySelectorAll(".reveal");
 
 const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("visible");
+  entries => {
+
+    entries.forEach(entry => {
+
+      if(entry.isIntersecting){
+
+        entry.target.classList.add("active");
+
       }
+
     });
+
   },
   {
-    threshold: 0.15,
+    threshold:0.15
   }
 );
 
-elements.forEach((element) => {
-  observer.observe(element);
+reveals.forEach(item => {
+  observer.observe(item);
 });
